dfcc03cf19981ccffe1147b6840f41d743548876
Mikael Kindborg <mikael.kindborg@gmail.com>
2017-01-30 20:28:11 +0100

    LoRa-demo: Added map and data for devices.

--- lora-mqtt/app.js
+++ lora-mqtt/app.js
@@ -3,8 +3,24 @@
 ;(function()
 {
 
+// Timestamp for sensor data that arrives over MQTT.
+var mCurrentTimeStamp = null
+
+// Object that holds sensor data.
+var mSensorData = {}
+
+// Map object.
+var mMap = null
+
+// Map marker.
+var mMapMarker = null
+
+// Initial values for device marker (Sodaq)
+var mLat = 59.452742
+var mLng = 18.289192
+
 // Timer that updates the displayed list of devices.
-//var mUpdateTimer = null
+var mUpdateTimer = null
 
 function main()
 {
@@ -42,6 +58,12 @@ function onDeviceReady()
   // Attach event listeners.
   $('.app-connect').on('click', onConnect)
   $('.app-disconnect').on('click', onDisconnect)
+
+  $('.app-show-sensors').on('click', onShowSensors)
+  $('.app-show-map').on('click', onShowMap)
+
+  // Show initial page.
+  //setTimeout(onShowMap, 100)
 }
 
 function onConnect()
@@ -158,6 +180,65 @@ function onDisconnect()
   hideDrawerIfVisible()
 }
 
+function onShowSensors()
+{
+  $('.app-page-map').hide()
+  $('.app-page-sensors').show()
+  hideDrawerIfVisible()
+}
+
+function onShowMap()
+{
+  $('.app-page-sensors').hide()
+  $('.app-page-map').show()
+  hideDrawerIfVisible()
+  loadMap()
+}
+
+function loadMap()
+{
+  if (mMap) return
+
+  mMap = L.map('my-map', { maxZoom: 25 })
+
+  /*
+  setTimeout(function()
+  {
+    mMap.setView([51.505, -0.09], 13)
+  //.setView(thingsLatLng, 20)
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: '&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mMap)
+  }, 1000)
+*/
+  
+  mMap.setView([mLat, mLng], 15)
+
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+		id: 'mapbox.streets'
+	}).addTo(mMap)
+
+  updateMapMarker()
+}
+
+function updateMapMarker()
+{
+  if (!mMap) return
+
+  if (!mMapMarker)
+  {
+    mMapMarker = L.marker([mLat, mLng]).addTo(mMap)
+  }
+  else
+  {
+    mMapMarker.setLatLng([mLat, mLng])
+  }
+}
+
 function hideDrawerIfVisible()
 {
   if ($('.mdl-layout__drawer').hasClass('mdl-layout__drawer is-visible'))
@@ -182,26 +263,48 @@ function drawGraph(container, data, title)
       {
           mode: 'time'
       },
-      selection:
+      /*selection:
       {
           mode: 'x'
-      },
+      },*/
       HtmlText: false,
       title: title
   }
   Flotr.draw(container, [data], options)
 }
 
-var mCurrentTimeStamp = null
-var mSensorData = {}
-
 function updateSensorData(payload)
 {
+  // Set current timestamp.
   mCurrentTimeStamp = new Date(payload.metadata.time).getTime()
-  updateSensor('message-counter', 'Messages', payload.counter)
-  updateSensor('rssi', 'RSSI', payload.metadata.gateways[0].rssi)
-  updateSensor('snr', 'SNR', payload.metadata.gateways[0].snr)
-  updateSensor('frequency', 'Frequency', payload.metadata.frequency)
+
+  // Check device id.
+  if (payload.dev_id == 'uno-rfclick-1')
+  {
+    try {
+      var x = JSON.parse(atob(payload.payload_raw)).x
+      updateSensor('sensor-x', 'Sensor X', x)
+    }
+    catch (error) {
+      console.log('Error parsing JSON payload (Uno): ' + error)
+    }
+    updateSensor('rssi', 'RSSI', payload.metadata.gateways[0].rssi)
+    updateSensor('snr', 'SNR', payload.metadata.gateways[0].snr)
+    updateSensor('frequency', 'Frequency', payload.metadata.frequency)
+  }
+
+  if (payload.dev_id == 'sodaq-onev2-1')
+  {
+    try {
+      var elements = atob(payload.payload_raw).split(' ')
+      mLat = Number(elements[0]) / 1000000
+      mLng = Number(elements[1]) / 1000000
+      updateMapMarker()
+    }
+    catch (error) {
+      console.log('Error parsing JSON payload (Sodaq): ' + error)
+    }
+  }
 }
 
 function updateSensor(sensorID, label, value)
@@ -212,8 +315,8 @@ function updateSensor(sensorID, label, value)
   // Add data point.
   mSensorData[sensorID].push([mCurrentTimeStamp, value])
 
-  // Keep max 100 data points.
-  if (mSensorData[sensorID].length > 100) mSensorData[sensorID].shift()
+  // Keep max 50 data points.
+  if (mSensorData[sensorID].length > 50) mSensorData[sensorID].shift()
   
   // Draw sensor data.
   drawGraph(
